0a4a95e92aa8b8f819edd1787b02e74f82987abe
techieYadav <106012202+techieYadav@users.noreply.github.com>
2022-05-23 17:50:35 +0530

    Update Find-the-Duplicate-Number.py

--- Find-the-Duplicate-Number.py
+++ Find-the-Duplicate-Number.py
@@ -1,3 +1,5 @@
+#Time Complexity: O(nlogn)
+#Space Complexity: O(logn) or O(n)
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         count = {}
@@ -6,3 +8,25 @@ class Solution:
                 count[i] = 1
             else:
                 return i
+ 
+
+#Time Complexity: O(n)
+#Space Complexity: O(n) 
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return num
+            seen.add(num)
+            
+            
+            
+#Time Complexity: O(n)
+#Space Complexity: O(1)            
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                return nums[i]
